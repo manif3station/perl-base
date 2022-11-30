fae54311390385b22483f38af7100cebc45734d3
Michael Vu <email@michael.vu>
2022-11-30 12:35:10 +0000

    chdir

--- Dockerfiles/service.pl
+++ Dockerfiles/service.pl
@@ -3,111 +3,6 @@
 use strict;
 use warnings;
 
-sub main {
-    my @cmd = @_;
-
-    _load_plugins();
-
-    _run_services();
-
-    @cmd ? _cmd( ENTRYPOINT => exec => join " ", @cmd )
-         : _cmd( CONTAINER_HOLDER => exec => qq{ sleep infinity } )
-         ;
-}
-
-sub _run_services {
-    foreach my $key ( sort keys %ENV ) {
-        if ( $ENV{DEBUG_SERVICE} ) {
-            print "[DEBUG] ENV KEY $key: $ENV{$key}\n";
-        }
-        if ( $key =~ /^IN_RUN_.+/ && _should_run($key)  ) {
-            my $cmd  = $ENV{$key} or next;
-            if ( $ENV{DEBUG_SERVICE} ) {
-                print "[DEBUG] IN RUN: $cmd\n";
-            }
-            _cmd( $key => system => $cmd );
-        }
-        elsif ( $key =~ /^JUST_RUN_.+/ && _should_run($key)  ) {
-            my $cmd  = $ENV{$key} or next;
-            if ( $ENV{DEBUG_SERVICE} ) {
-                print "[DEBUG] JUST RUN: $cmd\n";
-            }
-            _cmd( $key => background => $cmd );
-        }
-        elsif ( $key =~ /^KEEP_RUN_.+/ && _should_run($key) ) {
-            my $cmd  = $ENV{$key} or next;
-            if ( $ENV{DEBUG_SERVICE} ) {
-                print "[DEBUG] KEEP RUN: $cmd\n";
-            }
-            _cmd( $key => loop => $cmd );
-        }
-    }
-}
-
-sub _cmd {
-    my ( $label, $how, $cmd ) = @_;
-
-    printf "[%s] $label>> %s\n", _now(), $cmd;
-
-    if ( $how =~ /system/ ) {
-        system $cmd;
-    }
-    elsif ( $how =~ /background/ ) {
-        my $pid = fork;
-        return $pid if $pid;
-        local $0 = "SERVICE: $label";
-        exec $cmd;
-    }
-    elsif ( $how =~/loop/) {
-        my $pid = fork;
-        return $pid if $pid;
-        local $0 = "SERVICE: $label";
-        while (1) {
-            system "while [ true ]; do\n$cmd;\nsleep 5;\ndone";
-            sleep 5;
-        }
-    }
-    else {
-        local $0 = $label;
-        exec $cmd;
-    }
-}
-
-sub _should_run {
-    my ($key) = @_;
-    my $code = $ENV{"IF_$key"}
-        or return 1;
-    my $yes = eval $code;
-    my $error = $@;
-    warn "[X] $key ---> $error\n" if $error;
-    return $yes;
-}
-
-sub _now {
-    my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
-    return "$year-$mon-${mday}T$hour:$min:$sec";
-}
-
-sub _load_plugins {
-    my @keys = grep { /^SERVICE_PLUGIN_/ } keys %ENV;
-
-    foreach my $key(sort @keys) {
-        if ($key =~/CODE/) {
-            eval $ENV{$key};
-            warn "$key: $@" if $@;
-        }
-        elsif ($key =~/FILE/) {
-            if (-f $ENV{$key}) {
-                eval { require $ENV{$key} };
-                warn "$key: $@" if $@;
-            }
-            else {
-                warn "$key: file $ENV{$key} is not found.\n";
-            }
-        }
-    }
-}
-
 sub is_dir {
     my ($dir) = @_;
     return defined $dir && -d $dir;
@@ -125,7 +20,90 @@ sub is_empty {
 
 sub has_env {
     my ($key) = @_;
-    return grep { /$key/ && !is_empty($ENV{$_}) } keys %ENV;
+    return grep { /$key/ && !is_empty( $ENV{$_} ) } keys %ENV;
+}
+
+sub _now {
+    my ( $sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst ) =
+      localtime(time);
+    return "$year-$mon-${mday}T$hour:$min:$sec";
+}
+
+sub _chdir {
+    my ( $key, $code ) = @_;
+    my $pwd     = $ENV{PWD};
+    my $new_dir = $ENV{"CHDIR_$key"};
+    chdir $new_dir if $new_dir;
+    $code->();
+    chdir $pwd if $new_dir;
+}
+
+sub _cmd {
+    my ( $label, $how, $cmd ) = @_;
+
+    _chdir $label => sub {
+        printf "[%s] $label>> %s\n", _now(), $cmd if $ENV{RUN_VERBOSE};
+
+        if ( $how =~ /system/ ) {
+            system $cmd;
+        }
+        elsif ( $how =~ /background/ ) {
+            my $pid = fork;
+            return $pid if $pid;
+            local $0 = "SERVICE: $label";
+            exec $cmd;
+        }
+        elsif ( $how =~ /loop/ ) {
+            my $pid = fork;
+            return $pid if $pid;
+            local $0 = "SERVICE: $label";
+            while (1) {
+                system "while [ true ]; do\n$cmd;\nsleep 5;\ndone";
+                sleep 5;
+            }
+        }
+        else {
+            local $0 = $label;
+            exec $cmd;
+        }
+    };
+}
+
+sub _should_run {
+    my ($key) = @_;
+    my $code = $ENV{"IF_$key"}
+      or return 1;
+    my $yes   = eval $code;
+    my $error = $@;
+    warn "[X] $key ---> $error\n" if $error;
+    return $yes;
+}
+
+sub _run_services {
+    foreach my $key ( sort keys %ENV ) {
+        if ( $key =~ /^IN_RUN_.+/ && _should_run $key ) {
+            my $cmd = $ENV{$key} or next;
+            _cmd $key => system => $cmd;
+        }
+        elsif ( $key =~ /^JUST_RUN_.+/ && _should_run $key ) {
+            my $cmd = $ENV{$key} or next;
+            _cmd $key => background => $cmd;
+        }
+        elsif ( $key =~ /^KEEP_RUN_.+/ && _should_run $key ) {
+            my $cmd = $ENV{$key} or next;
+            _cmd $key => loop => $cmd;
+        }
+    }
+}
+
+sub main {
+    my @cmd = @_;
+
+    _run_services;
+
+    @cmd
+      ? _cmd( ENTRYPOINT       => exec => join " ", @cmd )
+      : _cmd( CONTAINER_HOLDER => exec => qq{ sleep infinity } );
 }
 
 caller or main(@ARGV);
