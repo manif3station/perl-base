7c0e7468c7ce914ab2a4cdc1229a2508a80b5a4c
Michael Vu <email@michael.vu>
2022-12-18 09:28:29 +0800

    Improved Installer Script for Dockerfiles
    The previous installer script had a few issues - it ran apt-get update every
    time it was run, even if it had already been run earlier in the process. This
    caused unnecessary delays in the build process. In addition, the script only
    supported bash scripts, but not perl scripts.
    
    To address these issues, the new installer script has been updated to only run
    apt-get update once at the very beginning. It also now supports both types of
    script - bash and perl - so that any type of installer can be run.
    
    Additionally, a big title has been added to the build log to clearly show which
    installer is being run at the moment, making it easier to track progress and
    troubleshoot any issues that may arise.

--- Dockerfiles/installer.pl
+++ Dockerfiles/installer.pl
@@ -3,12 +3,31 @@
 use strict;
 use warnings;
 
+system qw( chmod 777 /tmp );
+system qw( apt-get update );
+
 my @dirs = split /\n/, qx(find /app/src/plugins -type d -name Dockerfiles);
 
 foreach my $dir (sort @dirs) {
-    my $installer = "$dir/installer.sh";
-    next if !-f $installer;
-    print "Install $installer ...\n";
+    my ($installer) = grep { -f $_ } map { "$dir/installer.$_" } qw( sh pl )
+        or next;
+
+    next if $installer =~ m{perl-base/Dockerfiles/installer\.pl$};
+
     chdir $dir;
-    system bash => $installer;
+
+    my $title = "| Install $installer";
+    my $title_len = length $title;
+    my $width = $title_len + 3;
+
+    printf "%s\n", '-' x $width;
+    printf "%s%s |\n", $title, (' ' x ($width - (length $title) - 2));
+    printf "%s\n", '-' x $width;
+
+    if ($installer =~ m/\.sh$/) {
+        system bash => $installer;
+    }
+    elsif ($installer =~ m/\.pl$/) {
+        system perl => $installer;
+    }
 }
--- Dockerfiles/installer.sh
+++ Dockerfiles/installer.sh
@@ -1,7 +1,3 @@
-cd /app/src/plugins/perl-base/Dockerfiles
-
-apt-get update
-
 apt-get install -y apt-utils software-properties-common build-essential \
                    vim screen htop exuberant-ctags ntp net-tools ntpdate \
                    libxml2-dev libxslt1-dev rsync libnet-ssleay-perl \
